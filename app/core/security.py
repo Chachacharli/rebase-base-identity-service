b1fe66d1ce344873b6e23c6406b2146eed95d8a5
chachacharlie <carlos.torres@scisa.com>
2025-09-14 17:05:59 -0600

    ADD: Implement key generation and storage for RSA private and public keys

--- app/core/security.py
+++ app/core/security.py
@@ -1,24 +1,30 @@
-from jose import jwk
+from cryptography.hazmat.primitives import serialization
 from jose.utils import base64url_encode
 
-from app.core.config import settings
+from app.core.config import Settings
 
+path = Settings().PRIVATE_KEY_PATH
 
-def load_public_key():
-    with open(settings.PUBLIC_KEY_PATH, "rb") as f:
-        return f.read()
+# Cargar tu llave privada desde el archivo .pem
+with open(str(path), "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+    )
+
+public_key = private_key.public_key()
 
 
 def get_jwk():
-    """Convierte la clave pública en formato JWK para /.well-known/jwks.json"""
-    public_key = load_public_key()
-    jwk_key = jwk.construct(public_key, algorithm=settings.JWT_ALG)
+    numbers = public_key.public_numbers()
+    e = base64url_encode(numbers.e.to_bytes((numbers.e.bit_length() + 7) // 8, "big"))
+    n = base64url_encode(numbers.n.to_bytes((numbers.n.bit_length() + 7) // 8, "big"))
 
     return {
-        "kty": jwk_key._params["kty"],
-        "n": base64url_encode(jwk_key._params["n"]),
-        "e": base64url_encode(jwk_key._params["e"]),
-        "alg": settings.JWT_ALG,
+        "kty": "RSA",
         "use": "sig",
-        "kid": "auth-service-key-1",
+        "alg": "RS256",
+        "kid": "102f6d18-6089-43e7-b01e-cbb74c6bb68d",
+        "n": n.decode("utf-8"),
+        "e": e.decode("utf-8"),
     }
