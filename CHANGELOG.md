21c51b70b7af8d4de0b3332d317e417ddbedc1f9
chachacharlie <carlos.torres@scisa.com>
2025-09-20 17:09:43 -0600

    ADD: Update version to 0.2.0 and create CHANGELOG.md for project documentation

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# rebase-base-identity-service
+
+## 0.2.0 - 20/09/2025
+
+### Summary
+The necessary logic is added to return an HTML template with `Jinja2Templates` in order to post the end customer's password and username. 
+
+### Improvements
+- Implementation of HTML templates using `Jinja2Templates` for rendering login forms.
+- Enhanced user experience with a structured login page.
+
+### Corrections
+//
+
+### Decrements
+- The last access grand_type without an username and password is deprecated.
--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+# Import information of pyproject.toml
+import toml
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 
 from app.api import api_router
 from app.core.db import init_db
+
+pyproject_data = toml.load("pyproject.toml")
+__version__ = pyproject_data["tool"]["poetry"]["version"]
+__name__ = pyproject_data["tool"]["poetry"]["name"]
+__description__ = pyproject_data["tool"]["poetry"]["description"]
 
 
 # -----------------------------
@@ -15,28 +22,23 @@ from app.core.db import init_db
 async def lifespan(app: FastAPI):
     # Se ejecuta al iniciar la app
     init_db()
-    print("Tablas creadas o actualizadas según los modelos")
     yield
     # Aquí podrías poner lógica de cierre (shutdown)
-    print("Aplicación cerrando...")
+    print("Closing app...")
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan, title=__name__, version=__version__, description=__description__
+)
 
 templates = Jinja2Templates(directory="app/templates")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000", "https://rework-rate.scisa.com.mx"],
+    allow_origins=["http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get("/login")
-def login_form(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
 
 app.include_router(api_router)
