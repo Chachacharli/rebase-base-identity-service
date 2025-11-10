95b171c707f129ee0717de24c7a457ce4af4891f
chachacharlie <carlos.torres@scisa.com>
2025-11-09 19:14:48 -0600

    ADD: implement PermissionService for managing permissions

--- app/api/v1/permission.py
+++ app/api/v1/permission.py
@@ -6,29 +6,31 @@ from sqlmodel import Session
 from app.core.db import get_session
 from app.repositories.permission_repository import PermissionRepository
 from app.schemas.permission import PermissionCreate, PermissionRead, PermissionUpdate
+from app.services.permission_service import PermissionService
 
 router = APIRouter(prefix="/v1/permissions")
 
 
 @router.get("/")
 def list_permissions(session: Session = Depends(get_session)):
-    permission_repo = PermissionRepository(session)
-    return permission_repo.get_permissions()
+    permission_service = PermissionService(PermissionRepository(session))
+    permissions = permission_service.get_permissions()
+    return permissions
 
 
 @router.post("/")
 def create_permission(
     permission: PermissionCreate, session: Session = Depends(get_session)
 ):
-    permission_repo = PermissionRepository(session)
-    db_permission = permission_repo.create(permission)
+    permission_service = PermissionService(PermissionRepository(session))
+    db_permission = permission_service.create_permission(permission)
     return PermissionRead.model_validate(db_permission)
 
 
 @router.get("/{permission_id}")
 def get_permission(permission_id: UUID, session: Session = Depends(get_session)):
-    permission_repo = PermissionRepository(session)
-    db_permission = permission_repo.get_by_id(permission_id)
+    permission_service = PermissionService(PermissionRepository(session))
+    db_permission = permission_service.get_permission(permission_id)
     return PermissionRead.model_validate(db_permission)
 
 
@@ -38,8 +40,8 @@ def update_permission(
     permission: PermissionUpdate,
     session: Session = Depends(get_session),
 ):
-    permission_repo = PermissionRepository(session)
-    db_permission = permission_repo.update(permission_id, permission)
+    permission_service = PermissionService(PermissionRepository(session))
+    db_permission = permission_service.update_permission(permission_id, permission)
     return PermissionRead.model_validate(db_permission)
 
 
